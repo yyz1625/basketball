55f0ed03ae4a259773dff28a93325fdac96983fb
yyz1625 <1036565461@qq.com>
2020-01-20 00:59:09 +0800

    第一次提交

---
+++ src/main/java/com/jd/basketballcourt/BasketballcourtApplication.java
@@ -0,0 +1,18 @@
+package com.jd.basketballcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * yyz
+ * @Date 
+ */
+public class BasketballcourtApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BasketballcourtApplication.class, args);
+    }
+
+}
